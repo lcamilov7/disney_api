ba8a2e9d5497d140a5b296dc5c0f48512fc88686
Luis Camilo <luiscamilov@hotmail.com>
2024-02-02 10:39:41 -0500

    se supone ya esta listo el setup

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
+  get "up" => "rails/health#show", as: :rails_health_check
+
+end
